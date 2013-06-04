f7eb5f890723847e0a43ca603bd51bee63e9d5f8
FORLINI <spockit.inc@gmail.com>
2013-06-04 21:22:24 +0200

    map

--- proto/public/js/velib.js
+++ proto/public/js/velib.js
@@ -56,7 +56,6 @@ type = "dqf";
 
 var map = L.mapbox.map('map', 'etiwiti.map-91mhirzp')
     .setView([48.856, 2.342], 13);
-map.attributionControl.removeFrom(map);
 console.log(map);
 /* SOCKET */
 var socket = io.connect('http://localhost');
